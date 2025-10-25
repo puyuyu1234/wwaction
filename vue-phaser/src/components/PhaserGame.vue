1a971ff80079196844cb477f3e506165a61a5934
puyuyu1234 <ag5jm@proton.me>
2025-10-25 12:25:52 +0900

    [feat] vueに移植・とりあえず移動

---
+++ vue-phaser/src/components/PhaserGame.vue
@@ -0,0 +1,33 @@
+<template>
+  <div ref="gameContainer" class="game-container"></div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, onUnmounted, ref } from "vue";
+import { initGame } from "@/game/main";
+import type Phaser from "phaser";
+
+const gameContainer = ref<HTMLDivElement>();
+let game: Phaser.Game | null = null;
+
+onMounted(() => {
+  if (gameContainer.value) {
+    game = initGame(gameContainer.value);
+  }
+});
+
+onUnmounted(() => {
+  if (game) {
+    game.destroy(true);
+    game = null;
+  }
+});
+</script>
+
+<style scoped>
+.game-container {
+  width: 100%;
+  height: 100%;
+  background-color: #000;
+}
+</style>
